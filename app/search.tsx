4056ccf287ed1ffa384e3141f678229ec611bb49
marcuspahlman <mpahlmans@gmail.com>
2024-12-03 17:52:38 +0100

    REmove unused styles and add TODO comment for scroll

--- app/search.tsx
+++ app/search.tsx
@@ -1,27 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
 import SearchPresenter from "../presenters/SearchPresenter";
 
 export default function SearchScreen() {
   return (
-    // TODO: Implement Presenter
-    <SearchPresenter />
+    // TODO: Allow scroll to bottom
+    <View style={styles.container}>
+      {/* <View style={{ height: "92%" }}>  */}
+      <SearchPresenter />
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
   },
 });
