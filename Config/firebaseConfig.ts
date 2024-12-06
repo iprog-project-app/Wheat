3198986a8465d2993333951e2169f20191e51369
Victor <victor.fredrikson.01@gmail.com>
2024-12-06 13:45:31 +0100

    Fix environment variables

--- Config/firebaseConfig.ts
+++ Config/firebaseConfig.ts
@@ -1,17 +1,14 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.WHEAT_FIREBASE_APIKEY,
-  authDomain: process.env.WHEAT_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.WHEAT_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.WHEAT_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.WHEAT_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.WHEAT_FIREBASE_APP_ID
+  apiKey: process.env.EXPO_PUBLIC_WHEAT_FIREBASE_APIKEY,
+  authDomain: process.env.EXPO_PUBLIC_WHEAT_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.EXPO_PUBLIC_WHEAT_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_WHEAT_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_WHEAT_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_WHEAT_FIREBASE_APP_ID
 };
   
 
