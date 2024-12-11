8faa3ed5a903cb40a3d9c0699f3c67ef34a30d53
Samuel De Luna <smadeluna@gmail.com>
2024-12-11 21:10:27 +0100

    Sign in config

--- Config/firebaseConfig.ts
+++ Config/firebaseConfig.ts
@@ -1,6 +1,11 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
+import {
+  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_WHEAT_FIREBASE_APIKEY,
@@ -8,12 +13,11 @@ const firebaseConfig = {
   projectId: process.env.EXPO_PUBLIC_WHEAT_FIREBASE_PROJECT_ID,
   storageBucket: process.env.EXPO_PUBLIC_WHEAT_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.EXPO_PUBLIC_WHEAT_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_WHEAT_FIREBASE_APP_ID
+  appId: process.env.EXPO_PUBLIC_WHEAT_FIREBASE_APP_ID,
 };
-  
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
 export const db = getFirestore(app);
-
-
--- utilities/firebaseModel.ts
+++ utilities/firebaseModel.ts
@@ -1,33 +1,23 @@
-import { collection, addDoc } from "firebase/firestore";
-import { db } from "@/Config/firebaseConfig"; 
+import { collection, addDoc, setDoc, doc } from "firebase/firestore";
+import { db } from "@/Config/firebaseConfig";
+import { PlaceFullSchema } from "@/constants/types";
 
 interface user {
-    name: string,
-    email: string,
-    imgUrl: string,
-    friends: string[],
-    favourites: {
-        id: string; // place_id
-        title: string;
-        imageUri: string;
-        rating: number;
-        location: string;
-        isLiked: boolean;
-        note?: string;
-        description: string;
-        price: '$' | '$$' | '$$$' | '$$$$';
-        website: string;
-    }[] 
+  name: string;
+  email: string;
+  imgUrl: string;
+  friends: string[];
+  favourites: PlaceFullSchema[];
+  recentSearches: PlaceFullSchema[];
 }
 
-export const addUser = async (user: user) => {
-    try {
-        const docRef = await addDoc(collection(db, "users"), {
-            ...user
-        });
-        console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-        console.error("Error adding document: ", e);
-    }
-} 
-    
+export const addUser = async (user: user, uid: string) => {
+  try {
+    await setDoc(doc(db, "users", uid), {
+      ...user,
+    });
+    console.log("Document written!");
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+};
