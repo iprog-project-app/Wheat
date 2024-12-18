d12440e93784f8c9ca93a61ea060055210fee1be
marcuspahlman <mpahlmans@gmail.com>
2024-12-18 16:29:19 +0100

    Add FriendSearchPresenter and FriendSearchView for friend search functionality

---
+++ presenters/FriendSearchPresenter.tsx
@@ -0,0 +1,89 @@
+import { FriendSchema, PlaceFullSchema } from "@/constants/types";
+import SearchView from "../views/SearchView";
+import { useStore } from "@/store/model";
+import { router } from "expo-router";
+import { friendsSearch } from "@/utilities/firebaseModel";
+import { useState } from "react";
+import FriendSearchView from "@/views/FriendSearchView";
+import { Alert } from "react-native";
+
+export default function FriendSearchPresenter() {
+  // const setActiveUserData = useStore((state) => state.setActiveUserData); // TODO
+  const addFriend = useStore((state) => state.addFriend);
+  const removeFriend = useStore((state) => state.removeFriend);
+  const { setActiveFriendData } = useStore();
+  const { friendSearchQuery, setFriendSearchQuery } = useStore();
+  const { friendSearchResultsData, setFriendSearchResultsData } = useStore();
+  const { friends } = useStore();
+  const { isFriend } = useStore();
+
+  // TODO: Move to store/model.ts as its used multiple times
+  const sortResults = (results: FriendSchema[]) => {
+    return results.sort((a, b) => a.name.localeCompare(b.name));
+  };
+
+  const updateSearch = (search: string) => {
+    setFriendSearchQuery(search);
+  };
+
+  const toggleActiveData = (data: FriendSchema) => {
+    setActiveFriendData(data);
+    router.push("/friendProfile");
+  };
+
+  // TODO:handle toggle follow
+
+  const handleSearch = async () => {
+    try {
+      // Call the fetchPlacesByTextSearch function and await the result
+      const friendsResults = await friendsSearch(friendSearchQuery);
+      console.log("Friends results: ", friendsResults);
+      if (friendsResults) {
+        setFriendSearchResultsData(friendsResults);
+      }
+    } catch (error) {
+      console.error("Error fetching places:", error);
+    }
+  };
+
+  const handleToggleFollow = (user: FriendSchema) => {
+    console.log("Toggling follow for user: ", user);
+    if (isFriend(user.userId)) {
+      handleRemoveFriend(user);
+    } else {
+      console.log("Adding friend: ", user);
+      addFriend({ ...user });
+    }
+  };
+
+  const handleRemoveFriend = (user: FriendSchema) => {
+    Alert.alert(
+      "Remove Friend",
+      `Are you sure you want to stop following ${user.name}?`,
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Remove",
+          style: "destructive",
+          onPress: () => {
+            removeFriend(user.userId);
+          },
+        },
+      ]
+    );
+  };
+
+  return (
+    <FriendSearchView
+      searchQuery={friendSearchQuery}
+      searchResults={sortResults(friendSearchResultsData)}
+      onChangeText={updateSearch}
+      onToggleFollow={handleToggleFollow}
+      onPressItem={toggleActiveData}
+      onSearch={handleSearch}
+    />
+  );
+}
---
+++ views/FriendSearchView.tsx
@@ -0,0 +1,135 @@
+import { FriendSchema } from "@/constants/types";
+import { SearchBar } from "../components/SearchBar";
+import { FlatList, StyleSheet, View, Text } from "react-native";
+import Colors from "../constants/Colors";
+import { useMemo } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { FriendListItem } from "@/components/FriendListItem";
+import useStore from "@/store/model";
+
+export interface FriendSearchViewProps {
+  searchResults: FriendSchema[];
+  searchQuery: string;
+  onChangeText: (searchQuery: string) => void;
+  onToggleFollow: (user: FriendSchema) => void;
+  onPressItem: (user: FriendSchema) => void;
+  onSearch: () => void;
+}
+
+const SearchBarComponent = ({
+  searchQuery,
+  onChangeText,
+  onSubmit,
+}: {
+  searchQuery: string;
+  onChangeText: (text: string) => void;
+  onSubmit: () => void;
+}) => (
+  // TODO: Keyboard does not persist taps. Maybe add above FlatList: https://stackoverflow.com/questions/62148855/flatlist-search-bar-does-not-persist-keyboard-react-native
+  <SearchBar
+    key="searchBar"
+    placeholder="Find friends"
+    autoFocus
+    value={searchQuery}
+    onChangeText={onChangeText}
+    containerStyle={{
+      padding: 16,
+      backgroundColor: Colors.white,
+      borderTopWidth: 0,
+      borderBottomWidth: 0,
+    }}
+    inputContainerStyle={{
+      backgroundColor: Colors.gray5,
+      borderRadius: 8,
+      borderCurve: "continuous",
+    }}
+    onSubmitEditing={() => onSubmit()}
+  />
+);
+
+const EmptyState = () => (
+  <View style={styles.emptyContainer}>
+    <Ionicons name="person-add" size={40} color={Colors.gray2} />
+    <Text style={styles.emptyText}>{"Start searching to find friends!"}</Text>
+  </View>
+);
+
+export default function FriendSearchView({
+  searchResults,
+  searchQuery,
+  onChangeText,
+  onToggleFollow,
+  onPressItem,
+  onSearch,
+}: FriendSearchViewProps) {
+  const headerComponent = useMemo(
+    () => (
+      <SearchBarComponent
+        searchQuery={searchQuery}
+        onChangeText={onChangeText}
+        onSubmit={onSearch}
+      />
+    ),
+    [searchQuery, onChangeText]
+  );
+  const { isFriend } = useStore();
+
+  return (
+    // TODO: Allow scroll to bottom
+    <FlatList
+      style={{
+        backgroundColor: Colors.white,
+      }}
+      contentContainerStyle={{ marginBottom: 32 }}
+      data={searchResults}
+      ListHeaderComponent={headerComponent}
+      stickyHeaderIndices={[0]}
+      ListEmptyComponent={<EmptyState />}
+      renderItem={({ item }) => (
+        <FriendListItem
+          onPressItem={() => onPressItem(item)}
+          onToggleFollow={() => onToggleFollow(item)}
+          following={isFriend(item.userId)}
+          {...item}
+        />
+      )}
+      keyExtractor={(item) => item.userId}
+      ItemSeparatorComponent={() => (
+        <View
+          style={{ height: 1, width: "100%", backgroundColor: Colors.gray4 }}
+        />
+      )}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    gap: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+  emptyContainer: {
+    paddingTop: "30%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 24,
+    gap: 16,
+  },
+  emptyText: {
+    fontSize: 16,
+    color: Colors.gray2,
+    textAlign: "center",
+  },
+});
