49609b7d739bb643b3c023a9ce2bcf123e608138
marcuspahlman <mpahlmans@gmail.com>
2024-12-18 16:26:38 +0100

    Refactor FriendsPresenter and FriendsView to integrate friend management features and improve UI interactions

--- presenters/FriendsPresenter.tsx
+++ presenters/FriendsPresenter.tsx
@@ -1,13 +1,23 @@
 import FriendsView from "@/views/FriendsView";
 import { Alert } from "react-native";
-import { friendList } from "@/store/model";
+import useStore from "@/store/model";
+import { router } from "expo-router";
+import { FriendSchema } from "@/constants/types";
 
 export default function FriendsPresenter() {
-  const userFriendList = friendList;
-  const handleRemoveFriend = (id: string) => {
+  const { friends, addFriend, removeFriend } = useStore();
+
+  const handleToggleFollow = (user: FriendSchema) => {
+    if (friends.find((friend) => friend.userId === user.userId)) {
+      handleRemoveFriend(user);
+    }
+    addFriend({ ...user });
+  };
+
+  const handleRemoveFriend = (user: FriendSchema) => {
     Alert.alert(
       "Remove Friend",
-      "Are you sure you want to remove this friend?",
+      `Are you sure you want to stop following ${user.name}?`,
       [
         {
           text: "Cancel",
@@ -17,16 +27,27 @@ export default function FriendsPresenter() {
           text: "Remove",
           style: "destructive",
           onPress: () => {
-            // TODO: Implement remove friend
-            console.log("Remove friend with id: ", id);
+            removeFriend(user.userId);
           },
         },
-      ],
-      { cancelable: false }
+      ]
     );
   };
 
+  const handleAddFriends = () => {
+    router.push("/friendSearch");
+  };
+
+  const handlePressItem = (user: FriendSchema) => {
+    console.log("Sett profile modal visible. ID: ", user.userId);
+  };
+
   return (
-    <FriendsView friendsList={userFriendList} onRemove={handleRemoveFriend} />
+    <FriendsView
+      friendsList={friends}
+      onPressItem={handlePressItem}
+      onToggleFollow={handleToggleFollow}
+      onPressSearchFriends={handleAddFriends}
+    />
   );
 }
