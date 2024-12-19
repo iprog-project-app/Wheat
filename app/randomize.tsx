f1000bf91c1bf2c924b47101dc04a30c6e6ef10a
marcuspahlman <mpahlmans@gmail.com>
2024-12-19 13:41:00 +0100

    rename ModalScreen to RandomizeScreen and enhance layout with StatusBar

--- app/randomize.tsx
+++ app/randomize.tsx
@@ -1,8 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { Platform, StyleSheet, View } from "react-native";
 import RandomizePresenter from "@/presenters/RandomizePresenter";
 
-export default function ModalScreen() {
+export default function RandomizeScreen() {
   return (
-    // TODO: Implement Presenter
-    <RandomizePresenter />
+    // TODO: Allow scroll to bottom
+    <View style={styles.container}>
+      {/* <View style={{ height: "92%" }}>  */}
+      <RandomizePresenter />
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
